d106a93e3a95aebfdf0c522669dfb19a17269a90
rohitarer <rohitarer00@gmail.com>
2025-05-21 22:49:35 +0530

    api file

---
+++ toonara/lib/config/api_keys.dart
@@ -0,0 +1,10 @@
+/// Replace the placeholder strings below with your actual keys.
+/// Keep this file local and do NOT upload it to GitHub or public repos.
+library;
+
+const String geminiApiKey = ""; // 🔑 Your Gemini API Key
+const String huggingFaceToken = ""; // 🔐 Your Hugging Face Token
+const String replicateApiKey = '';
+// api_keys.dart
+const String deepAiApiKey = '';
+const String stableDiffusionApiKey = '';
